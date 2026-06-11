dfdeda046d6bae0d2b2b00e05194dc59502966f4
cush <oisin923@gmail.com>
2026-06-11 22:19:03 +0100

    Added skin upload preview

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("MC Skin Workshop Loaded");
+const upload = document.getElementById("skinUpload");
+const preview = document.getElementById("skinPreview");
+
+upload.addEventListener("change", function () {
+
+    const file = this.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        preview.src = event.target.result;
+        preview.style.display = "block";
+    };
+
+    reader.readAsDataURL(file);
+});
